3b575562126937486d5ad033a580c007647c60ea
Victoria  Topham <topham.victoria@gmail.com>
2020-02-27 08:34:08 -0800

    added stretch items

--- index.js
+++ index.js
@@ -158,6 +158,10 @@ class Instructor extends Lambdasian {
     return `${student.name} receives a perfect score on ${subject}`;
   }
 
+  gradeRandom(student) {
+    student.grade += Math.random() * 100 - 50;
+  }
+
 }
 
 /*
@@ -181,6 +185,7 @@ class Student extends Lambdasian{
     this.previousBackground = inputObj.previousBackground;
     this.className = inputObj.className;
     this.favSubjects = inputObj.favSubjects;
+    this.grade = 90;
   }
 
   listSubjects() {
@@ -195,7 +200,17 @@ class Student extends Lambdasian{
     return `${this.name} has begun spring challenge on ${subject}`;
   }
 
+  graduate() {
+    if (this.grade > 70) {
+      return `You have graduated, congratulations!`;
+    } else {
+      this.grade += 10;
+    }
+  }
+
 }
+
+
 
 /*
   TASK 6
@@ -228,9 +243,9 @@ class ProjectManager extends Instructor{
 
 /*
   STRETCH PROBLEM (no tests!)
-    - Extend the functionality of the Student by adding a prop called grade and setting it equal to a number between 1-100.
-    - Now that our students have a grade build out a method on the Instructor (this will be used by _BOTH_ instructors and PM's) that will randomly add or subtract points to a student's grade. _Math.random_ will help.
-    - Add a graduate method to a student.
+    DONE- Extend the functionality of the Student by adding a prop called grade and setting it equal to a number between 1-100.
+    DONE - Now that our students have a grade build out a method on the Instructor (this will be used by _BOTH_ instructors and PM's) that will randomly add or subtract points to a student's grade. _Math.random_ will help.
+    DONE - Add a graduate method to a student.
       + This method, when called, will check the grade of the student and see if they're ready to graduate from Lambda School
       + If the student's grade is above a 70% let them graduate! Otherwise go back to grading their assignments to increase their score.
 */
